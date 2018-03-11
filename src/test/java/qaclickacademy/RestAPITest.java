1b16525de45802b87860c2ad209c06c131fb7f90
Niza <nizakm@gmail.com>
2018-03-11 00:18:00 -0500

    first commit

---
+++ src/test/java/qaclickacademy/RestAPITest.java
@@ -0,0 +1,16 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class RestAPITest {
+	@Test
+	
+	public void ApiValidation() {
+		System.out.println("Api validation");
+		
+	}
+	public void ElementsInApi(){
+		
+		System.out.println("Elements in api");
+	}
+}
---
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,19 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+@Test
+	
+	public void BrowserAutomation() {
+		System.out.println("Browser Automation");
+		
+	}
+	public void ElementsUI(){
+		
+		System.out.println("Elements UI");
+	}
+
+
+}
